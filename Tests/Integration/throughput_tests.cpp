cdf53f739712a3fb599bb538cb0247cf6d99eabd
kosieram <mitchkos21@gmail.com>
2019-09-03 20:23:31 -0500

    quick typo fix

--- Tests/Integration/throughput_tests.cpp
+++ Tests/Integration/throughput_tests.cpp
@@ -1,5 +1,5 @@
 // TODO: we should figure out what problem we want to use for throughput testing.
-// we will probably want a different problem for each chromosome type as well class
+// we will probably want a different problem for each chromosome type as well as
 // a few problems that excersise throughput with kchromosome creatures.
 // example test problems: sudoko, tsp, nqueens, cart pole, ect.
 int first_throughput_test() {
