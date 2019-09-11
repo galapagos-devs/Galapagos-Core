c9f222cbde270afbedd8a63f204495ee6de69c61
kosieram <mitchkos21@gmail.com>
2019-09-10 20:17:08 -0500

    exported all functions for population metadata

--- Source/galapagos.cpp
+++ Source/galapagos.cpp
@@ -58,6 +58,78 @@ GALAPAGOS_API double get_population_metadata_survival_rate(population_metadata* 
 	return population_metadata->survival_rate;
 }
 
+GALAPAGOS_API void set_population_metadata_distance_threshold(population_metadata* double distance_threshold) {
+	population_metadata->distance_threshold = distance_threshold;
+}
+
+GALAPAGOS_API double get_population_metadata_distance_threshold(population_metadata* population_metadata) {
+	return population_metadata->distance_threshold;
+}
+
+GALAPAGOS_API void set_population_metadata_cooperative_coevolution(population_metadata* population_metadata, bool cooperative_coevolution) {
+	population_metadata->cooperative_coevolution = cooperative_coevolution;
+}
+
+GALAPAGOS_API bool get_population_metadata_cooperative_coevolution(population_metadata* population_metadata) {
+	return population_metadata->cooperative_coevolution;
+}
+
+GALAPAGOS_API void set_population_metadata_num_selection_algorithm_metadata(population_metadata* population_metadata, size_t num_selection_algorithm_metadata) {
+	population_metadata->num_selection_algorithm_metadata = num_selection_algorithm_metadata;
+}
+
+GALAPAGOS_API size_t get_population_metadata_num_selection_algorithm_metadata(population_metadata* population_metadata) {
+	return population_metadata->num_selection_algorithm_metadata;
+}
+
+GALAPAGOS_API void set_population_metadata_selection_algorithm_metadata(population_metadata* population_metadata, selection_algorithm_metadata* selection_algorithm_metadata) {
+	population_metadata->selection_algorithm_metadata = selection_algorithm_metadata;
+}
+
+GALAPAGOS_API selection_algorithm_metadata* get_population_metadata_selection_algorithm_metadata(population_metadata* population_metadata) {
+	return population_metadata->selection_algorithm_metadata;
+}
+
+GALAPAGOS_API void set_population_metadata_num_termination_condition_metadata(population_metadata* population_metadata, size_t num_termination_condition_metadata) {
+	population_metadata->num_termination_condition_metadata = num_termination_condition_metadata;
+}
+
+GALAPAGOS_API size_t get_population_metadata_num_termination_condition_metadata(population_metadata* population_metadata) {
+	return population_metadata->num_termination_condition_metadata;
+}
+
+GALAPAGOS_API void set_population_metadata_termination_condition_metadata(population_metadata* population_metadata, termination_condition_metadata* termination_condition_metadata) {
+	population_metadata->termination_condition_metadata = termination_condition_metadata;
+}
+
+GALAPAGOS_API termination_condition_metadata* get_population_metadata_termination_condition_metadata(population_metadata* population_metadata) {
+	return population_metadata->termination_condition_metadata;
+}
+
+GALAPAGOS_API void set_population_metadata_fitness_function(population_metadata* population_metadata, fitness_func_t fitness_function) {
+	population_metadata->fitness_function = fitness_function;
+}
+
+GALAPAGOS_API fitness_func_t get_population_metadata_fitness_function(population_metadata* population_metadata) {
+	return population_metadata->fitness_function;
+}
+
+GALAPAGOS_API void set_population_metadata_num_chromosome_metadata(population_metadata* population_metadata, size_t num_chromosome_metadata) {
+	population_metadata->num_chromosome_metadata = num_chromosome_metadata;
+}
+
+GALAPAGOS_API size_t get_population_metadata_num_chromosome_metadata(population_metadata* population_metadata) {
+	return population_metadata->num_chromosome_metadata;
+}
+
+GALAPAGOS_API void set_population_metadata_chromosome_metadata(population_metadata* population_metadata, chromosome_metadata* chromosome_metadata) {
+	population_metadata->chromosome_metadata = chromosome_metadata;
+}
+
+GALAPAGOS_API chromosome_metadata* get_population_metadata_chromosome_metadata(population_metadata* population_metadata) {
+	return population_metadata->chromosome_metadata;
+}
+
 GALAPAGOS_API population* create_population(population_metadata* population_metadata)
 {
 	galapagos_session& session = galapagos_session::get_instance();
