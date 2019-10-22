98ecaa91fcdf871440ac7c0fc96ef130231782b5
kosieram <mitchkos21@gmail.com>
2019-10-22 18:50:36 -0500

    exposed simple population creature api

--- Source/galapagos.cpp
+++ Source/galapagos.cpp
@@ -1,7 +1,5 @@
 #include "API/galapagos.h"
 #include "API/Factory/genetic_factory.h"
-#include "API/stochastic.h"
-#include "stochastic.h"
 
 /**************************
 *****Galapagos Session*****
@@ -12,4 +10,32 @@ GALAPAGOS_API population* create_population(population_metadata* population_meta
 
 GALAPAGOS_API void delete_population(population* population) {
 	delete population;
+}
+
+GALAPAGOS_API size_t population_get_size(population* population) {
+    return population->get_size();
+}
+
+GALAPAGOS_API creature* population_get_creature(population* population, int i) {
+    return population->get_creature(i);
+}
+
+GALAPAGOS_API creature* population_get_optimal_creature(population* population) {
+    return population->get_optimal_creature();
+}
+
+GALAPAGOS_API void population_evolve(population* population) {
+    population->evolve();
+}
+
+
+/***************************
+*****Galapagos Creature*****
+***************************/
+GALAPAGOS_API double creature_get_fitness(creature* creature) {
+    return creature->get_fitness();
+}
+
+GALAPAGOS_API chromosome* creature_get_chromosome(creature* creature, std::string name) {
+    return creature->get_chromosome(name);
 }
