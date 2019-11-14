a1a3f93acb143c7d58a351c8f21fe87e41e631b5
ShaneKosieradzki <shanekos1@gmail.com>
2019-11-13 23:42:57 -0600

    Define vector_chromosome interface.

---
+++ Source/Chromosomes/vector_chromosome.h
@@ -0,0 +1,50 @@
+//
+// Created by Shane on 11/13/2019.
+//
+
+#ifndef _GALAPAGOS_VECTOR_CHROMOSOME_H_
+#define _GALAPAGOS_VECTOR_CHROMOSOME_H_
+
+#include "../API/chromosome.h"
+#include "../API/galapagos_metadata.h"
+
+struct vector_chromosome_metadata : chromosome_metadata {
+    uint norm_rank;
+    size_t size;
+};
+
+class vector_chromosome : chromosome {
+private:
+    uint _norm_rank; // k-value to be used with the k-norm defined in get_distance
+    size_t _size;
+
+public:
+    explicit vector_chromosome(vector_chromosome_metadata* metadata);
+
+    ~vector_chromosome() override;
+
+    double get_distance(chromosome* other) override;
+
+    //region Indexing
+
+    virtual double get_gene(size_t index) = 0;
+    virtual double* get_slice(size_t end_index) = 0;
+    virtual double* get_slice(size_t start_index, size_t end_index) = 0;
+    virtual double* get_slice(size_t start_index, size_t end_index, size_t step_size) = 0;
+
+    //endregion
+
+    //region Math Operations
+
+    virtual vector_chromosome* add (vector_chromosome* other) = 0;
+    virtual vector_chromosome* subtract (vector_chromosome* other) = 0;
+    virtual vector_chromosome* multiply(double scalar) = 0;
+
+    virtual vector_chromosome* cross (vector_chromosome* other) = 0;
+    virtual vector_chromosome* dot (vector_chromosome* other) = 0;
+
+    //endregion
+
+};
+
+#endif /* _GALAPAGOS_VECTOR_CHROMOSOME_H_ */
