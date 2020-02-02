005b17a9941a0c90adc6f9bb9600f3cbe0e7a187
kosieram <mitchkos21@gmail.com>
2020-02-02 13:42:14 -0600

    fixed cmake so project is buildable

--- Tests/Unit/vector_mutation_tests.cpp
+++ Tests/Unit/vector_mutation_tests.cpp
@@ -1,6 +1,6 @@
-#include "../../Source/Chromosomes/vector_chromosome.h"
-#include "../../Source/Mutations/gaussian_mutation.h"
-#include "../../Source/Mutations/randomization_mutation.h"
+#include "../../Source/Chromosomes/Vector/vector_chromosome.h"
+#include "../../Source/Chromosomes/Vector/gaussian_mutation.h"
+#include "../../Source/Chromosomes/Vector/randomization_mutation.h"
 
 #include "../catch.hpp"
 #include "../fakeit.hpp"
@@ -12,20 +12,20 @@ TEST_CASE("gaussian mutation invoked", "[unit][vector-chromosome][mutation][gaus
     double desired_gene_value = 3;
 
     double gaussian_mean = 0;
-    double gaussian_variance = 1;
+    double gaussian_standard_deviation = 1;
 
     Mock<stochastic> stochastic_mock;
     When(OverloadedMethod(stochastic_mock, rand_int, int(int))).AlwaysReturn(0);
-    When(Method(stochastic_mock, rand_gaussian, double(double, double))).AlwaysReturn(0);
+    When(Method(stochastic_mock, rand_gaussian)).AlwaysReturn(0);
     stochastic* mocked_stochastic = &stochastic_mock.get();
 
     Mock<vector_chromosome> chromosome_mock;
-    When(Method(chromosome_mock, gene_inf, ))
+    When(Method(chromosome_mock, gene_inf)).AlwaysReturn(0);
     vector_chromosome* mocked_chromosome = &chromosome_mock.get();
 
     gaussian_mutation_metadata* mutation_metadata = new gaussian_mutation_metadata();
     mutation_metadata->mean = gaussian_mean;
-    mutation_metadata->variance = gaussian_variance;
+    mutation_metadata->standard_deviation = gaussian_standard_deviation;
     mutation* mutation = new gaussian_mutation(mutation_metadata, mocked_stochastic);
 
     vector_chromosome* mutated_chromosome = (vector_chromosome*)mutation->invoke(mocked_chromosome);
