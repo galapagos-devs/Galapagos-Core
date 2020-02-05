81cbb0a846829e5e70cfc5c2aee6112d81c0f440
kosieram <mitchkos21@gmail.com>
2020-02-05 16:49:49 -0600

    create ut file for housing vector chromosome crossover tests

---
+++ Tests/Unit/vector_chromosome_crossover_tests.cpp
@@ -0,0 +1,7 @@
+#include "../../Source/Chromosomes/Vector/vector_chromosome.h"
+#include "../../Source/Chromosomes/Vector/kpoint_crossover.h"
+
+#include "../catch.hpp"
+#include "../fakeit.hpp"
+
+using namespace fakeit;
