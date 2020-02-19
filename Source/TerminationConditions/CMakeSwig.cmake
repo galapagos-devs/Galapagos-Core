708ed69006f3514be9ca34d439dc757f50f61251
ShaneKosieradzki <shanekos1@gmail.com>
2020-02-18 19:43:54 -0600

    Added swig file for SelectionAlgorithms and TerminationConditions

---
+++ Source/TerminationConditions/CMakeSwig.cmake
@@ -0,0 +1,14 @@
+set_source_files_properties(termination_conditions.swg PROPERTIES CPLUSPLUS ON)
+
+swig_add_library(termination_conditions_swig
+        LANGUAGE
+        python
+        OUTPUT_DIR
+        ${CMAKE_SOURCE_DIR}/bin/swig
+        OUTFILE_DIR
+        ${CMAKE_SOURCE_DIR}/bin/swig/sources
+        SOURCES
+        termination_conditions.swg
+        fitness_threshold.cpp)
+
+swig_link_libraries(termination_conditions_swig Python::Module Python::Python)
