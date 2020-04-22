84a02ef05dfd38d9a0f847f9c9d4de3847a0616b
ShaneKosieradzki <shanekos1@gmail.com>
2020-04-21 19:20:43 -0500

    Added Constructors to all the metadata structs in `galapagos_metadata.h`

--- Source/API/galapagos_metadata.h
+++ Source/API/galapagos_metadata.h
@@ -17,10 +17,10 @@ typedef std::function<void(log_entry_t*)> log_func_t;
 typedef std::function<double(creature*)> fitness_func_t;
 
 typedef struct genetic_operator_metadata {
-    virtual  ~genetic_operator_metadata() = default;
     const double weight;
 
     explicit genetic_operator_metadata(const double weight) : weight{weight} {};
+    virtual  ~genetic_operator_metadata() = default;
 } genetic_operator_metadata_t;
 
 typedef struct crossover_metadata : genetic_operator_metadata {
@@ -31,7 +31,6 @@ typedef struct mutation_metadata : genetic_operator_metadata {
     explicit mutation_metadata(const double weight) : genetic_operator_metadata{weight} {};
 } mutation_metadata_t;
 
-
 typedef struct selection_algorithm_metadata {
     virtual ~selection_algorithm_metadata() = default;
 } selection_algorithm_metadata_t;
@@ -41,20 +40,35 @@ typedef struct termination_condition_metadata {
 } termination_condition_metadata_t;
 
 typedef struct chromosome_metadata {
+	const std::string name;
+	const double crossover_rate;
+    const double mutation_rate;
+    const size_t num_crossovers;
+    const crossover_metadata_t** crossover_metadata;
+    const size_t num_mutations;
+	const mutation_metadata_t** mutation_metadata;
+
+    chromosome_metadata(
+            std::string name, const double crossover_rate, const double mutation_rate,
+            const size_t num_crossovers, const crossover_metadata_t** crossover_metadata,
+            const size_t num_mutations, const mutation_metadata_t** mutation_metadata) :
+                name{std::move(name)}, crossover_rate{crossover_rate}, mutation_rate{mutation_rate},
+                num_crossovers{num_crossovers}, crossover_metadata{crossover_metadata},
+                num_mutations{num_mutations}, mutation_metadata{mutation_metadata} {}
     virtual  ~chromosome_metadata() = default;
-	std::string name;
-	double crossover_rate;
-	double mutation_rate;
-	size_t num_crossovers;
-	crossover_metadata_t** crossover_metadata;
-	size_t num_mutations;
-	mutation_metadata_t** mutation_metadata;
+
 } chromosome_metadata_t;
 
 typedef struct creature_metadata {
-    fitness_func_t fitness_function;
-	size_t num_chromosomes;
-	chromosome_metadata_t** chromosome_metadata;
+    const fitness_func_t fitness_function;
+	const size_t num_chromosomes;
+	const chromosome_metadata_t** chromosome_metadata;
+
+    creature_metadata(
+            fitness_func_t fitness_function,
+            const size_t num_chromosomes, const chromosome_metadata_t** chromosome_metadata) :
+                fitness_function{std::move(fitness_function)},
+                num_chromosomes{num_chromosomes}, chromosome_metadata{chromosome_metadata} {}
 } creature_metadata_t;
 
 typedef struct population_metadata {
@@ -70,7 +84,7 @@ typedef struct population_metadata {
     const creature_metadata_t* creature_metadata;
 
     population_metadata(
-            log_func_t  log_function, const size_t size, const double survival_rate,
+            log_func_t log_function, const size_t size, const double survival_rate,
             const double distance_threshold, const bool cooperative_coevolution,
             const size_t num_selection_algorithms, const selection_algorithm_metadata_t** selection_algorithm_metadata,
             const size_t num_termination_conditions, const termination_condition_metadata_t** termination_condition_metadata,
