85f2e6191b7e3a18ae2b4e362f14eeb2b1a8e61c
Shane Kosieradzki <shanekos1@gmail.com>
2021-07-19 20:29:38 -0500

    Changes by mitch

--- Source/Chromosomes/Vector/randomization_mutation.cpp
+++ Source/Chromosomes/Vector/randomization_mutation.cpp
@@ -1,11 +1,12 @@
 #include "randomization_mutation.h"
 
-std::shared_ptr<chromosome> randomization_mutation::invoke(const std::shared_ptr<const vector_chromosome>& chromosome) const {
-    auto child = std::make_shared<vector_chromosome>(chromosome);
+// TODO: argument chromosome was renamed cchromosome to fix the type shadowing of the trailing return type. fix this.
+auto randomization_mutation::invoke(const std::shared_ptr<const vector_chromosome>& cchromosome) const -> std::shared_ptr<chromosome> {
+    auto child = std::make_shared<vector_chromosome>(cchromosome);
     double min = child->gene_inf();
     double max = child->gene_sup();
 
-    for(size_t i = 0; i < chromosome->num_genes(); i++) {
+    for(size_t i = 0; i < cchromosome->num_genes(); i++) {
         double value = _stochastic_instance.rand_double(min, max);
         child->set_gene(i, value);
     }
